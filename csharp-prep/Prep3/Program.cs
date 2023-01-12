9c7690c1ba26d8522c4e17ae14f2473700a08ac2
juan-zv <juanzuritavasquez@gmail.com>
2023-01-12 12:59:46 -0700

    updated prep program 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        int guess_num = 0;
+
+        Console.WriteLine("What is the magic number? ");
+        string magic_num = Console.ReadLine();
+        
+        while (guess_num != int.Parse(magic_num))
+        {
+            Console.WriteLine("What is your guess?");
+            guess_num = int.Parse(Console.ReadLine());
+
+            if (magic_num > guess_num)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
+
+def fhvjyv(x, y, f)
+    sum = x + y 
+
+
+
+fhvjyv()
