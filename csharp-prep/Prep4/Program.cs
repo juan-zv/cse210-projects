25ac52144ea265d9a21aefac12e71c61e4e153d0
juan-zv <juanzuritavasquez@gmail.com>
2023-01-12 13:44:18 -0700

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,8 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        int num_list = -1;
+        List<int> numbers = new List<int>();
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-        Console.WriteLine("Enter number: ");
-        num_list = int.Parse(Console.ReadLine());
+
+
+        do{
+            Console.WriteLine("Enter number: ");
+            num_list = int.Parse(Console.ReadLine());
+            numbers.Add(num_list);
+        } while (num_list != 0);
+
+        Console.WriteLine(numbers);
     }
 }
