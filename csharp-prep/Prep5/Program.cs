be1e0eb99976998d65f4053e7a6022174833eaf2
juan-zv <juanzuritavasquez@gmail.com>
2023-01-14 17:02:52 -0700

    fnished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,37 @@ using System;
 
 class Program
 {
+
+    static void DIsplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    static int SquareNumbers(int num)
+    {
+        int square = num * num;
+        return square;
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string name = PromptUserName();
+        int number = SquareNumbers(PromptUserNumber());
+        Console.WriteLine($"{name}, the square of your number is {number}");
     }
+
 }
