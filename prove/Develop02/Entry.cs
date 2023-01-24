efdde4b4a694e02b08300debef0047e1d5e4a4cc
juan-zv <juanzuritavasquez@gmail.com>
2023-01-24 13:36:15 -0700

    created files for classes

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+
+
+public class Entry 
+{
+    public string _entry = "";
+    public List<string> _entriesList = [];
+    public Entry (){
+
+    }
+
+    public void SaveEntry(){
+
+    }
+
+
+
+
+
+
+}
---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+
+
+public class Journal
+{
+
+
+
+
+
+
+
+}
