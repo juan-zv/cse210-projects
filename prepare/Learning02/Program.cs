b94ee1a3f75cf08ef2bf840c4387be7527c239cb
juan-zv <juanzuritavasquez@gmail.com>
2023-01-25 20:09:59 -0700

    finished program for resume :( it was hard

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -18,7 +18,12 @@ class Program
         job2._endYear = 2023;
 
 
-        
+        Resume myResume = new Resume();
+        myResume._name = "Alison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.display();
 
 
     }
