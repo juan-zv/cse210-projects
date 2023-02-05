5d04ced3f187d04cf64457380e078b70bad92d13
juan-zv <juanzuritavasquez@gmail.com>
2023-02-04 22:22:19 -0700

    seguimos

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -9,24 +9,55 @@ class Program
 
         Entry Entrada = new Entry();
 
-        Entrada.GetEntry();    
-
-        Lista.Add(Entrada);
-
-        Console.WriteLine(Lista[0]);
+        int option = 5;
 
 
+
+        do{ 
         Console.WriteLine("Hello, this is the journal program. Please select one of the following or 0 to quit the program: ");
+        Console.WriteLine("0. Quit");
         Console.WriteLine("1. Write an Entry");
         Console.WriteLine("2. Display the entries");
         Console.WriteLine("3. Save entries to a file");
         Console.WriteLine("4. Load entries from a file");
+        Console.WriteLine("");
 
-        int option = int.Parse(Console.ReadLine());
+
+        option = int.Parse(Console.ReadLine());
 
         if (option == 1) {
+            Entrada.GetEntry();
+
+            Lista.Add(Entrada);
+            Console.WriteLine("");
+
             
         }
+
+        else if (option == 2) {
+            Console.WriteLine(Lista[0]);
+            Console.WriteLine("");
+
+        }
+
+        else if (option == 3) {
+            Console.WriteLine("Option 3");
+            Console.WriteLine("");
+        }
+
+
+        else if (option == 4) {
+            Console.WriteLine("Option 4");
+            Console.WriteLine("");
+
+        }
+
+        else {
+            Console.WriteLine("Please select a valid option");
+            Console.WriteLine("");
+        }
+
+        } while (option != 0);
 
     }
 }
